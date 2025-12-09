5923d266b3c1356f5cc3496279ecbed1dd64f87f
Jalolov_Manguberdi <62201299+Developer06092000@users.noreply.github.com>
2025-12-09 10:18:44 +0500

    123

--- src/TestStart.jsx
+++ src/TestStart.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TestStart = ({ changeStatus }) => {
   return (
     <div className="test_start">
-      <h3>Fan: Davlat xaridlari</h3>
+      <h3>Fan: G'aznachilik</h3>
       <p>Mazkur test 25 ta savoldan iborat bo'lib, ishlash uchun 30 daqiqa vaqt ajratilgan.</p>
       <button className="btn btn_end" onClick={() => changeStatus("test")}>
         boshlash
